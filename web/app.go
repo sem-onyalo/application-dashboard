570abc237dcada9720b854de36d1047f2d4d5e9e
Sem Onyalo <oonyalo@gmail.com>
2018-01-26 13:55:35 -0500

    added asset handler in web app service for serving static resources (css, js)

--- web/app.go
+++ web/app.go
@@ -1,17 +1,19 @@
 package web
 
 import (
+	"bufio"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 	"strconv"
-
-	"github.com/sem-onyalo/application-dashboard/service/response"
+	"strings"
 
 	"github.com/sem-onyalo/application-dashboard/service"
 	"github.com/sem-onyalo/application-dashboard/service/request"
+	"github.com/sem-onyalo/application-dashboard/service/response"
 )
 
 // TODO: move to config service
@@ -85,6 +87,7 @@ func (a App) Start(request request.StartApp) response.StartApp {
 // setRoutes sets the HTTP API routes
 func (a App) setRoutes() {
 	http.HandleFunc("/", a.rootHandler)
+	http.HandleFunc("/assets/", a.assetHandler)
 	http.HandleFunc(fmt.Sprintf("/api/v%s", webAPIVersion), a.rootAPIHandler)
 	http.HandleFunc(fmt.Sprintf("/api/v%s/endpoints", webAPIVersion), a.endpointsHandler)
 	http.HandleFunc(fmt.Sprintf("/api/v%s/endpoints/tests", webAPIVersion), a.endpointsTestHandler)
@@ -96,6 +99,31 @@ func (a App) rootHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
+	}
+}
+
+// assetHandler handles static resource files
+func (a App) assetHandler(w http.ResponseWriter, r *http.Request) {
+	var contentType string
+	if strings.HasSuffix(r.URL.Path, ".css") {
+		contentType = "text/css"
+	} else if strings.HasSuffix(r.URL.Path, ".js") {
+		contentType = "text/javascript"
+	} else {
+		contentType = "text/plain"
+	}
+
+	templatesDirConfig := a.Config.GetValue(request.GetConfigValue{Key: webAppTemplatesDirConfigKey})
+	file, err := os.Open(templatesDirConfig.Value + r.URL.Path)
+	if err != nil {
+		// TODO: send to log service
+		fmt.Printf("Error opening url path %s: %s\\n", r.URL.Path, err)
+		w.WriteHeader(404)
+	} else {
+		defer file.Close()
+		w.Header().Set("Content-Type", contentType)
+		reader := bufio.NewReader(file)
+		reader.WriteTo(w)
 	}
 }
 
